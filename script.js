c45e778b8077886e46272719c3c917d78636847e
Sarah Cabral <>
2022-10-23 23:08:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var nome_usuario = document.getElementById("nome_usuario");
+var email_usuario = document.getElementById("email_usuario");
+var numero_usuario = document.getElementById("numero_usuario");
+
+document.getElementById("enviar_botao").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+  if(nome_usuario.value != "" && email_usuario.value != "" && numero_usuario.value != ""){
+    alert("Prontinho! Você receberá as novidades por email.");
+  }
+  else{
+    alert("Por favor, preencha os campos nome, email e telefone!");
+  }
+}

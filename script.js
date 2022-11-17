3166def4683b3ab5c543353bd385c3504a58206a
Sarah Cabral <87037373+Sar000inha@users.noreply.github.com>
2022-11-17 12:38:56 +0000

    Alteração na cor dos tópicos do menu principal.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var nome_usuario = document.getElementById("nome_usuario");
 var email_usuario = document.getElementById("email_usuario");
 var numero_usuario = document.getElementById("numero_usuario");
 
-document.getElementById("enviar_botao").addEventListener("click", validaFormulario)
+document.getElementById("enviar_botao").addEventListener("click", validaFormulario);
 
 function validaFormulario(){
   if(nome_usuario.value != "" && email_usuario.value != "" && numero_usuario.value != ""){
